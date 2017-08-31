46630ef79daacf3bc34a7b243bd87207c5e2c4f1
almazi <navilalmazi@gmail.com>
2017-08-31 21:49:07 +0600

    39th Problem solution: 1059

---
+++ Beginner/src/_1059.java
@@ -0,0 +1,14 @@
+/**
+ * Created by almazi on 8/31/17, 9:44 PM.
+ */
+public class _1059 {
+    public static void main(String[] args) {
+        int even = 1;
+        while (even <= 100){
+            if (even % 2 == 0){
+                System.out.println(even);
+            }
+            even++;
+        }
+    }
+}
