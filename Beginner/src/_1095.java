1ecc1ea3cb9c29300fc5b207f27f41f0caed5d38
almazi <navilalmazi@gmail.com>
2017-09-01 11:23:08 +0600

    56th Problem solution: 1095

---
+++ Beginner/src/_1095.java
@@ -0,0 +1,13 @@
+/**
+ * Created by almazi on 9/1/17, 11:16 AM.
+ */
+public class _1095 {
+    public static void main(String[] args) {
+        int i = 1, j = 60;
+        while(j >= 0){
+            System.out.printf("I=%d J=%d\n",i,j);
+            i += 3;
+            j -= 5;
+        }
+    }
+}
